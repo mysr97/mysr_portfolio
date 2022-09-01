2c4d7c14df74f803d47bd07eb224c3a8feb79ae0
mysr97 <104114396+mysr97@users.noreply.github.com>
2022-09-01 14:42:28 +0900

    .

--- js/sori_one.js
+++ js/sori_one.js
@@ -1,7 +1,24 @@
+// page1 스크롤 버튼 호버시 마우스 바뀌게
 $(function(){
+  $(".p1_scroll > img").hover(function(){
+    $(".cursor").css("opacity","0");
+  },function(){
+    $(".cursor").css("opacity","1");
+  });
+
   $(".p1_scroll").hover(function(){
     $(".cursor").css("opacity","0");
   },function(){
     $(".cursor").css("opacity","1");
+  });
+});
+
+
+// page1 스크롤 버튼 애니메이션
+$(document).ready(function ($) {
+  $(".p1_scroll").click(function (event) {
+      event.preventDefault();
+      $('html,body').animate({ scrollTop: $(".page2").offset().top }, 700);
+
   });
 });
