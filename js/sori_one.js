dcb0e6bbc77f9d3bdbe245c0687d15787ab37fd7
mysr97 <104114396+mysr97@users.noreply.github.com>
2022-09-02 12:42:54 +0900

    page2 수정

--- js/sori_one.js
+++ js/sori_one.js
@@ -19,6 +19,45 @@ $(document).ready(function ($) {
   $(".p1_scroll").click(function (event) {
       event.preventDefault();
       $('html,body').animate({ scrollTop: $(".page2").offset().top }, 700);
-
   });
+});
+
+
+
+//컬러 변경
+
+function rgbToHex ( rgbType ){ 
+  /* 
+  ** 컬러값과 쉼표만 남기고 삭제하기. 
+  ** 쉼표(,)를 기준으로 분리해서, 배열에 담기. 
+  */ 
+  var rgb = rgbType.replace( /[^%,.\d]/g, "" ).split( "," ); 
+  
+  rgb.forEach(function (str, x, arr){ 
+  
+      /* 컬러값이 "%"일 경우, 변환하기. */ 
+      if ( str.indexOf( "%" ) > -1 ) str = Math.round( parseFloat(str) * 2.55 ); 
+      
+      /* 16진수 문자로 변환하기. */ 
+      str = parseInt( str, 10 ).toString( 16 ); 
+      if ( str.length === 1 ) str = "0" + str; 
+      
+      arr[ x ] = str; 
+  }); 
+  
+  return "#" + rgb.join( "" ); 
+};
+
+$(document).ready(function () {
+  $(".page2 .p2_wr ul li").mouseover(function () {
+    var color = $(this).children();
+    var color2 = color.css("color");
+    var colorhex = rgbToHex(color2);
+    var colorch = colorhex.substring(0, 7);
+
+  console.log(color2)
+    $(this).children().fadeIn("slow");
+    $(this).css("color",colorch);
+    $(this).children().css("color",colorch);
+  })
 });
